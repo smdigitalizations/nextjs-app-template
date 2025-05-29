acaa64b4f2491b08c43700bc32ae538e11e1902c
smdigitalizations <smdigitalizations@gmail.com>
2025-05-29 08:57:18 +0700

    Add .next/static/chunks/src_app_assignments_new_page_tsx_45db58bd._.js 1748483837596

---
+++ .next/static/chunks/src_app_assignments_new_page_tsx_45db58bd._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_assignments_new_page_tsx_2c4034c4._.js"
+],
+    source: "dynamic"
+});
