ce0d0efbc92deba253c419ab50d1d0de8b1309bd
smdigitalizations <smdigitalizations@gmail.com>
2025-05-29 08:56:43 +0700

    Add .next/static/chunks/src_app_dashboard_page_tsx_b1824a07._.js 1748483802554

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_b1824a07._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_97acc635._.js",
+  "static/chunks/src_5b6c0e37._.js"
+],
+    source: "dynamic"
+});
