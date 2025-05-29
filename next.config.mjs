8df00a9edf0c6e0c4d3a69d0c6f47ce9ca8e06f6
smdigitalizations <smdigitalizations@gmail.com>
2025-05-29 08:56:25 +0700

    Add next.config.mjs 1748483785480

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  transpilePackages: ['react-big-calendar'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    })
+    return config
+  },
+}
+
+export default nextConfig
